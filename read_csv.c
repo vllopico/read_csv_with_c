7b3717a3a5a9a4cd5e7c7816d0770045ed41dad4
Vicent Llopico <70440969+vllopico@users.noreply.github.com>
2021-10-31 09:39:23 +0100

    Update read_csv.c

--- read_csv.c
+++ read_csv.c
@@ -9,46 +9,46 @@ int main(void)
 {
 	clock_t  begin = clock();
 	
-    FILE* file = fopen("data.csv", "r");
+    	FILE* file = fopen("data.csv", "r");
     
-    if(file == NULL) {
-      printf("Error en el fitxer!");   
-      exit(1);             
+    	if(file == NULL) {
+      		printf("Error en el fitxer!");   
+      		exit(1);             
 	}
 	    
-    char line[LLINEA];
-    char *tok;
-    char *endv;
+    	char line[LLINEA];
+    	char *tok;
+    	char *endv;
     
-    double valor;
-    double vd;
+    	double valor;
+    	double vd;
     
-    //Variables fila
-    double sumfila = 0.0;
-    double avgfila = 0.0;
-    int elsfila;
-    int numfila = 0;
-    double maxfila = 0.0;
-    double minfila = 0.0;
+    	//Variables fila
+    	double sumfila = 0.0;
+    	double avgfila = 0.0;
+    	int elsfila;
+    	int numfila = 0;
+    	double maxfila = 0.0;
+    	double minfila = 0.0;
     
     
-    //Variables totals
-    double sumtotal = 0.0;
-    double avgtotal = 0.0;
-    int elstotal = 0;
-    double maxtotal = 0.0;
-    double mintotal = 0.0;
+    	//Variables totals
+    	double sumtotal = 0.0;
+    	double avgtotal = 0.0;
+    	int elstotal = 0;
+    	double maxtotal = 0.0;
+    	double mintotal = 0.0;
     
-    printf("\n");
+    	printf("\n");
 	printf("%-15s %-15s %-15s %-15s %-15s %-15s\n","Fila","Elements","Sumatori","Mitja","Max","Min");
 	printf("=================================================================================================\n");
 	
-    while (fgets(line, sizeof(line), file)) {
+    	while (fgets(line, sizeof(line), file)) {
 
 		if(line[0] != '\r' && line[0] != '\n'){
-	    	
+			
 			sumfila = 0.0;
-	    	elsfila = 0;
+	    		elsfila = 0;
 	    	
 			tok = strtok(line,";");
 			
@@ -103,25 +103,26 @@ int main(void)
 		
 		numfila = numfila + 1;
 		
-    }
-    if (elstotal == 0){
-    	avgtotal = 0.0;
+	}
+	
+	if (elstotal == 0){
+    		avgtotal = 0.0;
 	}
 	else {
 		avgtotal = sumtotal / elstotal;
 	}
     
-    printf("=================================================================================================\n");
+    	printf("=================================================================================================\n");
     
-    printf("\nTOTALS\n\n");
-    printf("%-15s %-15s %-15s %-15s %-15s %-15s\n","Files","Elements","Sumatori","Mitja","Max","Min");
+    	printf("\nTOTALS\n\n");
+    	printf("%-15s %-15s %-15s %-15s %-15s %-15s\n","Files","Elements","Sumatori","Mitja","Max","Min");
 	printf("=================================================================================================\n");
 	printf("%-15d %-15d %-15f %-15f %-15f %-15f\n",numfila,elstotal,sumtotal,avgtotal,maxtotal,mintotal);
 	printf("=================================================================================================\n");
     
-    fclose(file);
-    clock_t end = clock();
-    double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
+    	fclose(file);
+    	clock_t end = clock();
+    	double time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
 	printf("\nTEMPS SCRIPT: %lf segons\n", time_spent);
     
 	return 0;
